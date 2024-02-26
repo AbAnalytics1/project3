80b02f90dd55de2d7329fa064baa517e33781772
Abena Donkor <108335529+AbAnalytics1@users.noreply.github.com>
2024-02-26 21:26:57 +0000

    Update car sales analysis.sql

--- car sales analysis.sql
+++ car sales analysis.sql
@@ -604,7 +604,7 @@ LEFT JOIN manufacturer m
 ON s.manufacturer_id = m.manufacturer_id
 
 
--- what are the top five manufacturer that acquired the highest sales
+-- what are the top five manufacturer that acquired the highest.
 
 SELECT TOP(5) m.manufacturer_name, SUM(S.sales) AS revenue_generated
 FROM sales s
